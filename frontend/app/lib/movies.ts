85ec19a58449d61076f424e27167795fd6e84475
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2026-05-13 17:20:56 -0300

    feat: fetch rated movies and sendVote functions

--- frontend/app/lib/movies.ts
+++ frontend/app/lib/movies.ts
@@ -6,6 +6,8 @@ import {
   fetchInfoSchema,
   fetchInfoResultSchema,
   apiErrorSchema,
+  sendVoteSchema,
+  ratedMoviesResultSchema,
 } from "~/schemas/movies";
 
 const API_BASE_URL = import.meta.env.VITE_API_ENDPOINT;
@@ -79,4 +81,70 @@ export async function fetchMovieInfo(
   }
 
   return null;
+}
+
+export async function fetchRatedMovies(): Promise<
+  z.infer<typeof ratedMoviesResultSchema> | null
+> {
+  try {
+    const response = await fetch(`${API_BASE_URL}/movies/rated`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      const parsed = ratedMoviesResultSchema.safeParse(await response.json());
+      if (parsed.success) {
+        return parsed.data;
+      } else {
+        console.error(parsed.error);
+      }
+    } else {
+      try {
+        const parsed = apiErrorSchema.safeParse(await response.json());
+        if (parsed.success) {
+          console.error("Erro:", parsed.data.error);
+        }
+      } catch {
+        console.error("Erro:", "Erro desconhecido");
+      }
+    }
+  } catch (error) {
+    console.error("Erro:", error);
+  }
+
+  return null;
+}
+
+export async function sendVote(data: z.infer<typeof sendVoteSchema>): Promise<boolean> {
+  try {
+    const response = await fetch(`${API_BASE_URL}/vote`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        movie_id: data.MovieId,
+        user_rating: data.Rating,
+      }),
+    });
+
+    if (response.ok) {
+      return true;
+    } else {
+      try {
+        const parsed = apiErrorSchema.safeParse(await response.json());
+        if (parsed.success) {
+          console.error("Erro:", parsed.data.error);
+        }
+      } catch {
+        console.error("Erro:", "Erro desconhecido");
+      }
+    }
+  } catch (error) {
+    console.error("Erro:", error);
+  }
+
+  return false;
 }
