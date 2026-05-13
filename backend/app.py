50da08b6d4f7613de4f77d243c37e3007250dffd
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2026-05-13 17:19:05 -0300

    feat: vote and fetch rated movies

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 import os
 
+import psycopg2
 import requests
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 
@@ -13,6 +15,14 @@ CORS(app)
 
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+)
 
 DEFAULT_LANG = "pt-BR"
 
@@ -113,6 +123,16 @@ def get_movie(movie_id: int):
     except requests.RequestException as err:
         return jsonify({"error": f"Request failed: {str(err)}"}), 503
 
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT rating from user_ratings WHERE id = %s",
+                (movie_id,),
+            )
+            user_rating = cur.fetchone()
+    except Exception as err:
+        return jsonify({"error": str(err)}), 500
+
     return jsonify(
         {
             "id": data["id"],
@@ -122,6 +142,7 @@ def get_movie(movie_id: int):
             "release_date": data.get("release_date", ""),
             "runtime": data.get("runtime", 0),
             "vote_average": data.get("vote_average", 0.0),
+            "user_vote": user_rating[0] if user_rating[0] is not None else -1.0,
             "genres": [g["name"] for g in data.get("genres", [])],
             "poster_url": (
                 f"https://image.tmdb.org/t/p/w500{data['poster_path']}"
@@ -132,5 +153,50 @@ def get_movie(movie_id: int):
     )
 
 
+@app.route("/vote", methods=["POST"])
+def vote():
+    # validating input
+    try:
+        data = request.get_json()
+    except Exception as err:
+        return jsonify({"error": str(err)}), 500
+
+    movie_id = int(data.get("movie_id"))
+    user_rating = float(data.get("user_rating"))
+
+    if movie_id is None or user_rating is None:
+        return jsonify({"error": "Must provide movie_id and user_rating"}), 400
+
+    if user_rating > 10 or user_rating < 0:
+        return jsonify({"error": "rating must be between 0 and 10"}), 400
+
+    # database insertion
+    try:
+        with conn.cursor() as cur:
+            cur.execute(
+                """INSERT INTO user_ratings (id, rating) VALUES (%s, %s)
+                ON CONFLICT (id) DO UPDATE SET rating = EXCLUDED.rating""",
+                (movie_id, user_rating),
+            )
+        conn.commit()
+    except Exception as err:
+        conn.rollback()
+        return jsonify({"error": str(err)}), 500
+
+    return jsonify("success")
+
+
+@app.route("/movies/rated", methods=["GET"])
+def get_rated_movies():
+    try:
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT id, rating from user_ratings")
+            rows = cur.fetchall()
+    except Exception as err:
+        return jsonify({"error": str(err)}), 500
+
+    return jsonify({"results": rows})
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
